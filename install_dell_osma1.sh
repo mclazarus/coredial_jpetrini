b8cb2fee4be651710ce14fa396eeca3bf0b9e5ba
John Petrini <john.d.petrini@gmail.com>
2014-09-15 16:56:41 -0400

    "Added feature to remove install script from server once complete"

--- install_dell_osma1.sh
+++ install_dell_osma1.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 #- ***********************************
-#- *        parallel_scp v1.0        *
+#- *     install_dell_osma1 v1.0     *
 #- * Copyright (C) 2014 John Petrini *
 #- ***********************************
 
-## ***************************************************************
-## * Usage: ./parallel_scp.sh [filetocopy] [destinationlist] *
-## *                                                             *
-## *      -h     Show help options.                              *
-## *      -v     Print version info.                             *
-## *                                                             *
-## *     DESTINATION LIST MUST CONTAIN FQDN's - ONE PER LINE     *
-## ***************************************************************
+## *****************************************************************
+## * Usage: ./install_dell_osma1.sh [filetocopy] [destinationlist] *
+## *                                                               *
+## *      -h     Show help options.                                *
+## *      -v     Print version info.                               *
+## *                                                               *
+## *     DESTINATION LIST MUST CONTAIN FQDN's - ONE PER LINE       *
+## *****************************************************************
 help=$(grep "^## " "${BASH_SOURCE[0]}" | cut -c 4-)
 version=$(grep "^#- "  "${BASH_SOURCE[0]}" | cut -c 4-)
  
@@ -35,5 +35,5 @@ MACHINES="$(<$2)"
 
 for current_machine in $MACHINES; do 
    scp -i ~/.ssh/id_dsa_va -o "StrictHostKeyChecking no" "$FILE" voiceaxis@$current_machine:$TARGET_PATH 
-   ssh -t -i ~/.ssh/id_dsa_va -o "StrictHostKeyChecking no" voiceaxis@$current_machine './install_dell_osma_centos5.sh && exit'
+   ssh -t -i ~/.ssh/id_dsa_va -o "StrictHostKeyChecking no" voiceaxis@$current_machine './install_dell_osma_centos5.sh && rm install_dell_osma_centos5.sh && exit'
 done
