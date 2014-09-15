c59830a55837f70c5b2d9ce7d24a953ac6c9b717
John Petrini <john.d.petrini@gmail.com>
2014-09-15 16:20:25 -0400

    Initial Commit

---
+++ install_dell_osma1.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#- ***********************************
+#- *        parallel_scp v1.0        *
+#- * Copyright (C) 2014 John Petrini *
+#- ***********************************
+
+## ***************************************************************
+## * Usage: ./parallel_scp.sh [filetocopy] [destinationlist] *
+## *                                                             *
+## *      -h     Show help options.                              *
+## *      -v     Print version info.                             *
+## *                                                             *
+## *     DESTINATION LIST MUST CONTAIN FQDN's - ONE PER LINE     *
+## ***************************************************************
+help=$(grep "^## " "${BASH_SOURCE[0]}" | cut -c 4-)
+version=$(grep "^#- "  "${BASH_SOURCE[0]}" | cut -c 4-)
+ 
+opt_h() {
+  echo "$help"
+}
+ 
+opt_v() {
+  echo "$version"
+}
+ 
+while getopts "hv" opt; do
+  eval "opt_$opt"
+exit 0
+done
+
+FILE=$1
+TARGET_PATH="/home/voiceaxis"
+MACHINES="$(<$2)"
+
+echo $MACHINES
+
+for current_machine in $MACHINES; do 
+   scp -i ~/.ssh/id_dsa_va -o "StrictHostKeyChecking no" "$FILE" voiceaxis@$current_machine:$TARGET_PATH 
+   ssh -t -i ~/.ssh/id_dsa_va -o "StrictHostKeyChecking no" voiceaxis@$current_machine './install_dell_osma_centos5.sh && exit'
+done
