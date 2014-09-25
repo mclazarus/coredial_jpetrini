090d13de25f173a85268a590bf529d35be064914
John Petrini <john.d.petrini@gmail.com>
2014-09-25 13:44:41 -0400

    Delete rollback_dell_osma.sh

--- rollback_dell_osma.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#### Dell OMSA and SNMP Uninstall 09/16/2014 jpetrini. 
-
-sudo yum -y remove srvadmin-base srvadmin-idrac7 srvadmin-idracadm7 srvadmin-storageservices ipmitool srvadmin-omcommon
-
-sudo yum -y remove net-snmp-utils
-
-
